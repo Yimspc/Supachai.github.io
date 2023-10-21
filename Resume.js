ab8d04742926f22104400a754b506f596615b873
Yimspc <s6401072510338@email.kmutnb.com>
2023-10-21 23:11:34 +0700

    first

---
+++ Resume.js
@@ -0,0 +1,22 @@
+function greet() {
+    alert("Welcom in Resume");
+}
+
+function thip() {
+    var image = document.getElementById("hiddenthip");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }
+}
+
+function siem() {
+    var image = document.getElementById("hiddensiem");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }
+}
+
